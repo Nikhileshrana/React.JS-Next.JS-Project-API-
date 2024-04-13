ddb624b3386a933476d5e2d701cb4c023edb466d
Nikhilesh Rana <realnikhileshrana@gmail.com>
2024-04-13 15:44:37 +0530

    HUe HUe

---
+++ Components/Header.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div id='header-container'>
+        <div>
+            <div>
+                LOGO
+            </div>
+        </div>
+        <div>
+            <div>PRODUCTS</div>
+            <div>ABOUT US</div>
+            <div>CONTACT US</div>
+            <div>LOGIN</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default Header
--- app/page.js
+++ app/page.js
@@ -1,13 +1,22 @@
+// https://learnreact-rho.vercel.app/
+
 "use client"
+
 import React, { useState } from 'react'
+import Footer from "@/Components/Footer"
+import Header from "@/Components/Header"
 
 const page = () => {
   const [marks, setmarks] = useState(80);
   return (
     <>
+    <Header></Header>
+
     <div>Your Marks are  : {marks}</div>
     <button onClick={()=>{setmarks(90)}}>Update Marks to 90</button>
     <button onClick={()=>{setmarks(80)}}>Revert The Action</button>
+    
+    <Footer></Footer>
     </>
   )
 }
